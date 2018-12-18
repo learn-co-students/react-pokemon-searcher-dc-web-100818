f2be34ee26dd6313222d834921b9f3ceabd2e66c
github username <github email address>
2018-12-18 12:45:10 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,12 +4,21 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
+    let searchTerm = this.props.userInput
+    let filtered = this.props.pokemons.filter(searchedPokemon => searchedPokemon.name.includes(searchTerm))
+
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+
+
+      {filtered.map(pokemon => <PokemonCard key={pokemon.id} pokemonObj={pokemon}/>)}
+
+
       </Card.Group>
+
     )
   }
 }
 
 export default PokemonCollection
+  // {this.props.pokemons.map(pokemon => <PokemonCard key={pokemon.id} pokemonObj={pokemon}/>)}
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,49 @@ class PokemonForm extends React.Component {
     }
   }
 
+  onFormChanges = (e) =>{
+    let targetName = e.target.name
+    let targetValue = e.target.value
+    this.setState({
+      [targetName]: targetValue
+    })
+
+
+    console.log("changing", targetName, targetValue)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let data = {
+      name: this.state.name,
+      stats: [
+      {value: this.state.hp,
+        name: 'hp'
+      }],
+
+      sprites: {front: this.state.frontUrl,
+        back: this.state.backUrl}
+    }
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(newData => this.props.addNewPokemon(newData))
+
+    console.log("submitted")
+  }
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.onFormChanges}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.onFormChanges} />
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.onFormChanges}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.onFormChanges}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
